e710fda5b6e4bf1e065037ad2808867b3b481bfe
sheridkala <sheridkala@myvuw.ac.nz>
2024-02-07 11:32:40 +1300

    Max can only be ZZ

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import string
 
-alphabet = string.ascii_uppercase
+def generator(max:int, file, prefix:str="", pi:int=0, i:int=0):
+    realMax = 26**2 + 26 # This is not ideal, I want to go forever!
 
-def generator(prefix, i, max, file):
-    if i > max:
-        return
+    alphabet = string.ascii_uppercase
+
     for n in range(26):
-        if i > max:
+        if i >= max or i >= realMax:
             return
         s = prefix + alphabet[n] + "\n"
         i += 1
         file.write(s)
-        generator(prefix + alphabet[n], i, max, file)
+
+    generator(max, file, alphabet[pi], (pi+1)%26, i)
 
 # -------------------
 
 file = open("thingo.txt", "w")
 
-generator("", 0, 702, file)
+generator(71000, file)
 
 file.close()
