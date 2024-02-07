56afe530a07796d47f6359d08ae3ce9e1aa05096
sheridkala <sheridkala@myvuw.ac.nz>
2024-02-07 14:37:58 +1300

    Completed

--- main.py
+++ main.py
@@ -2,30 +2,32 @@ import string
 
 def generator(max:int, file, alphabet = string.ascii_uppercase):
     mod = len(alphabet)
-    piList = [0]
+    iList = [0]
 
+    for n in range(max):
+        s = ""
+        for i in iList:
+            s += alphabet[i]
+        s += "\n"
 
+        file.write(s)
 
-    for i in range(max):
-        print(piList)
+        iList[-1] += 1
 
-        piList[-1] += 1
+        for j in range(len(iList)-1, 0, -1):
+            if iList[j] == mod:
+                iList[j] = 0
+                iList[j-1] += 1
 
-        if len(piList) > 1:
-            for j in range(len(piList)-1, 1, -1):
-                if j == mod:
-                    piList[j] = 0
-                    piList[j-1] += 1
-
-        if piList[0] == mod:
-            for n in range(len(piList)):
-                piList[n] = 0
-            piList.append(0)
+        if iList[0] == mod:
+            for n in range(len(iList)):
+                iList[n] = 0
+            iList.append(0)
 
 # -------------------
 
 file = open("thingo.txt", "w")
 
-generator(1000, file)
+generator(6969, file)
 
 file.close()
